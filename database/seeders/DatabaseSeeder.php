aad2d2d32c38ca04a4f634b8ff3709c2b4d4bc2e
ZINEB <mahlil.zineb1997@gmail.com>
2023-05-22 16:25:41 +0000

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Client;
+use App\Models\Commande;
+use App\Models\Produit;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+   
+
+
+ public function run(): void
+    {
+        Client::factory()->count(10)->create()->each(
+            function ($client){
+                Commande::factory()->count(5)->create(
+                  [ 'client_id' => $client->id ])->each(
+                        function ($commande){
+                            $produits = Produit::factory()->count(rand(1, 5))->create();
+                            foreach ($produits as $produit) {
+                                $commande->produits()->attach($produit, [
+                                    'quantite' => rand(1, 10),
+                                ]);
+                            }
+                        }
+                );
+            }
+        );
+        
+    }
+}
